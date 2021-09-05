fe42b9edd5f6065043327b0b71caf1b89913275a
NOSIBBiswas22 <88088042+NOSIBBiswas22@users.noreply.github.com>
2021-09-05 23:06:44 +0600

    Delete footer.js

--- footer.js
+++
@@ -1,7 +0,0 @@
-//changing the inner html of the span with "footer_year" id to the given year(2021)
-
-function getCurrentYear() {
-    return new Date().getFullYear(); // returns the year via local timing
-  }
-  
-  document.getElementById("footer_year").innerHTML = getCurrentYear();
